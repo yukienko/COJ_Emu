5156a73c9b1722de6b6f3bda8f87e7dce7d3ce6f
Unknown <yukineko1442@gmail.com>
2019-08-05 23:38:16 +0900

    マウスでカードを移動できるようにした

---
+++ Assets/Game/Script/Card.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    private Vector3 oldV3;
+    private Vector3 moveTo;
+    private bool beRay = false;
+    new Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldV3 = transform.position;
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RayCheck();
+        }
+
+        if (beRay)
+        {
+            MovePoisition();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            beRay = false;
+            transform.position = oldV3;
+        }
+    }
+
+    private void RayCheck()
+    {
+        Ray ray = new Ray();
+        RaycastHit hit = new RaycastHit();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == gameObject.GetComponent<Collider>())
+        {
+            beRay = true;
+        }
+        else
+        {
+            beRay = false;
+        }
+
+    }
+
+    private void MovePoisition()
+    {
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+
+        moveTo = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = moveTo;
+
+    }
+}
