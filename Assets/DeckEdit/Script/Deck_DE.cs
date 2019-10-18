35391e2553da204398f337e119fb9049b8df4dfb
Unknown <yukineko1442@gmail.com>
2019-10-19 02:24:28 +0900

    ソート機能が完成した

--- Assets/DeckEdit/Script/Deck_DE.cs
+++ Assets/DeckEdit/Script/Deck_DE.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using System.Collections.ObjectModel;
+
 
 public class Deck_DE : MonoBehaviour
 {
 	public List<Card_DE> cardList = new List<Card_DE>();
+	public List<Transform> objList;
 
 	public void Add(Card_DE _card)
 	{
@@ -18,5 +23,30 @@ public class Deck_DE : MonoBehaviour
 		Card_DE card = cardList[_position];
 		cardList.Remove(card);
 		return card;
+	}
+
+	public void Sort()
+	{
+		//CardListのみだがカードid順にソート
+		//cardList.Sort((a, b) => a.section - b.section);
+		//cardList.Sort((a, b) => a.id - b.id);
+		//cardList.Sort((a, b) => a.id - b.id);
+		//cardList.Sort((a, b) => a.id - b.id);	
+
+		IOrderedEnumerable<Card_DE> sortList = cardList.OrderBy(b => b.section).ThenBy(a => a.id);
+		var count = cardList.Count;
+		Debug.Log(sortList.Count());
+		foreach(Card_DE card in sortList)
+		{
+			cardList.Add(card);
+		}
+		cardList.RemoveRange(0, count);
+
+		objList = cardList.ConvertAll(x => x.transform.GetComponentInChildren<Transform>());
+
+		foreach (var obj in objList)
+		{
+			obj.SetSiblingIndex(cardList.Count - 1);
+		}
 	}
 }
