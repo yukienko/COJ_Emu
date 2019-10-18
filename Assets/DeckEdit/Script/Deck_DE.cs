2ad0d82de312d811c6179d008fd53be8f9297579
Unknown <yukineko1442@gmail.com>
2019-10-19 03:43:13 +0900

    簡単なデッキセーブ機能の追加をした

--- Assets/DeckEdit/Script/Deck_DE.cs
+++ Assets/DeckEdit/Script/Deck_DE.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using System.Collections.ObjectModel;
-
+using System.IO;
+using UnityEngine.UI;
 
 public class Deck_DE : MonoBehaviour
 {
 	public List<Card_DE> cardList = new List<Card_DE>();
 	public List<Transform> objList;
+	public GameObject savePanel;
+	public Text deckName;
+	private const int deckCardLimit = 40;
+
 
 	public void Add(Card_DE _card)
 	{
@@ -25,14 +30,43 @@ public class Deck_DE : MonoBehaviour
 		return card;
 	}
 
-	public void Sort()
+	public void DeckSavePanel_Active()
 	{
-		//CardListのみだがカードid順にソート
-		//cardList.Sort((a, b) => a.section - b.section);
-		//cardList.Sort((a, b) => a.id - b.id);
-		//cardList.Sort((a, b) => a.id - b.id);
-		//cardList.Sort((a, b) => a.id - b.id);	
+		deckName.text = "";
+		savePanel.gameObject.SetActive(true);
+	}
 
+	public void DeckSavePanel_UnActive()
+	{
+		savePanel.gameObject.SetActive(false);
+	}
+
+	public void DeckSave()
+	{
+		//デッキ枚数が40枚か
+		if (cardList.Count == deckCardLimit)
+		{
+			DeckSort();
+			string deckFilePath = Environment.CurrentDirectory + "\\deckFile";
+			if (!Directory.Exists(deckFilePath))
+			{
+				Directory.CreateDirectory(deckFilePath);	
+			}
+			deckFilePath = deckFilePath + "\\" + deckName.text + ".txt";
+			var info = "DeckName:" + deckName.text;
+			File.WriteAllText(deckFilePath, info + "\n");
+			for (int i = 0; i < cardList.Count; i++)
+			{
+				var stream = cardList[i].id.ToString() + "," + cardList[i].section.ToString() + "\n";
+				File.AppendAllText(deckFilePath, stream);
+			}
+		}
+		Debug.Log("SaveOK");
+		DeckSavePanel_UnActive();
+	}
+
+	public void DeckSort()
+	{
 		IOrderedEnumerable<Card_DE> sortList = cardList.OrderBy(b => b.section).ThenBy(a => a.id);
 		var count = cardList.Count;
 		Debug.Log(sortList.Count());
@@ -48,5 +82,10 @@ public class Deck_DE : MonoBehaviour
 		{
 			obj.SetSiblingIndex(cardList.Count - 1);
 		}
+	}
+
+	private void Start()
+	{
+		savePanel.SetActive(false);	
 	}
 }
